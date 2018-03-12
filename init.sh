b0bd229dbe644d3ab0301189617e957bb2b005ae
m4kvn <fun.masahiro@gmail.com>
2018-03-12 10:17:58 +0900

    Update init

--- init.sh
+++ init.sh
@@ -6,3 +6,7 @@ raw=raw/master
 curl -sL $github/install/$raw/setup.sh | sh
 curl -sL $github/scripts/$raw/setup.sh | sh
 curl -sL $github/dotfiles/$raw/setup.sh | sh
+
+sh ~/.install/installer/applications.sh
+sh ~/.install/installer/homebrew/zsh.sh
+sh ~/.install/installer/homebrew/zplug.sh
